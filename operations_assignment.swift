c52d17093dff2f5e3be2ffce3aaa98cc6537fbe9
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2025-02-11 14:14:47 -0600

    operation project

---
+++ operations_assignment.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var imatationInput1 = 10
+var imatationInput2 = 25
+var imatationInput3 = 65
+//Here i started the variables
+
+var multiplied = imatationInput1 * imatationInput1
+var divided = imatationInput3 / imatationInput1
+var added = imatationInput2 + imatationInput3
+var subtracted = imatationInput3 - imatationInput1
+var percentages = Float(imatationInput1) / Float(imatationInput3) * 100
+//I set up my operations
+
+print(multiplied)
+print(divided)
+print(added)
+print(subtracted)
+print(percentages)
+//Printing the results.
+//changing the values still comes out properly.
