196da92b0d4f5587b235bb88011ba0fb45a17347
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2025-02-11 00:01:38 -0600

    constants_and_variables assignment

---
+++ constants_and_variables.swift
@@ -0,0 +1,20 @@
+var placesivebeen
+import UIKit
+let whereiwasborn = "Illinois"
+//let setting a constant that couldn't change
+var placesivebeen = "Illinois"
+// var making a variable that I can change and add to
+placesivebeen += " "
+placesivebeen += "Mexico"
+//I'm adding a space between the places in the string and adding new places I've been
+placesivebeen += " "
+placesivebeen += "Vegas"
+//Pretty much repeating it for this and the next set
+placesivebeen += " "
+placesivebeen += "Lake Erie"
+print(placesivebeen)
+print(whereiwasborn)
+//I ask it to print the constant which couldn't have changed and the variable that changed throughout.
+//below is just thoughts i had after writing this as to where this could be improved in the future with new learned skills.
+//If i knew how to ask for user input and a loop this would be a functional app other than
+// needing to make it a list obviously.
