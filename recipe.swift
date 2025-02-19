79ca81c40f3e7bf3a14a5a0b5cc2ca1673cf3af2
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2025-02-18 20:50:54 -0600

    recipeh

---
+++ recipe.swift
@@ -0,0 +1,16 @@
+// i'll need 
+// variables to hold seconds, minutes hours
+// a while loop to run while the clock is on
+// a stipulation of how to increment time properly ???(I dont know how yet)
+//an if statement to increment 60 seconds to 1 min and 60 min to 1 hr and to reset if we hit 24 hrs
+//a way to not have an infinite loop that makes the computer angry and explode.
+
+//recipe would be implementing the above tools in the right order
+
+
+//I did that wrong but I dont want to delete it
+//ingredients:if statements, while loops, variables to change += ops, true false booleans.
+//instructions:set variables. set the start of the clock with the while.
+//implement if statements for if a minute is reached set seconds to 0 and += minutes by 1
+//same for minutes to hours.
+//display time
