4e9b63e3bf0e4d18cc84f9f3735cf94610e8b890
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2025-02-25 13:46:41 -0600

    recipes folder and blackhawks recipe file

---
+++ recipes.swift/bh_recipe.swift
@@ -0,0 +1,8 @@
+//I will need... a dictionary,I'll need arrays to store information
+// I'll need loops to go through the values and opperators to average ages, 
+// values for each month, a lot of loops
+
+//innitialize values, make dictionary and arrays.
+//use loops to use these values to make a list sorted by age, a list sorted by country
+//calculate the average age and height of  the players using the lists and arrays.
+//figure it out.
