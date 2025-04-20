6bea5609ec429f53c607964cdca8ff793e47fc07
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2025-04-20 10:48:12 -0500

    update!

--- dokkanProject2.0.swift
+++ dokkanProject2.0.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 
-//Next objective for you is to learn and utilize Init to make the ability to "make" a unit, also for ease of implementing new units without needing 9+ lines for units.
+
 
 class Unit {
     var name: String = "name" //unit's name
@@ -14,6 +14,18 @@ class Unit {
     var passive2: Float = 1.0 //multiplicative part of their passive
     var superatk: Float = 1.0 //their super attack multiplier (Including stacks)
     
+    init(name: String, atk: Float, leaderSkill: Float, passive: Float, links: Float, item: Float, ki: Float, passive2: Float, superatk: Float) {
+        self.name = name
+        self.atk = atk
+        self.leaderSkill = leaderSkill
+        self.passive = passive
+        self.links = links
+        self.item = item
+        self.ki = ki
+        self.passive2 = passive2
+        self.superatk = superatk
+    }
+
     func calcAtkStat() -> Float {
         return dropDecimal(dropDecimal(dropDecimal(dropDecimal(dropDecimal(dropDecimal(dropDecimal(self.atk * self.leaderSkill) * self.passive) * self.links) * self.item) * self.ki) * self.passive2) * self.superatk)
     }
@@ -31,20 +43,8 @@ class Unit {
             
         return Int(averageDmgDealt)
         }
+
     }//this function returns the character's atk stat to be printed. It's math specifically wants to be done in such a way all decimal points between numbers are dropped. It looks a little messy as a result.
-
-func dropDecimal(_ num: Float) -> Float{ //updated naming scheme from drop_decimal to dropDecimal
-    return Float(Int(num))
-}//this function drops the decimal off floating numbers and returns them to being a float like the game does giving more close to accurate calculations and data
-import Foundation
-
-func formatNumbers(_ num: Int) -> String{
-    let formatter = NumberFormatter()
-    formatter.numberStyle = .decimal
-    var formattedNumber = formatter.string(from: NSNumber(value: num)) ?? "\(num)" //the question marks handle "Nil-Coalescing" to handle how NSNumber makes the number an optional
-    return String(formattedNumber)
-}//This function makes my own instance of NumberFormatter to use, sets it to .decimal then using Foundation and NSNumber it converts the number into a string with the commas.
-
 class Boss{
     var bossName: String = "name"
     var bossDef: Float = 1.0
@@ -53,57 +53,98 @@ class Boss{
     var bossDmg: Float = 1.0
     var bossDmgSuper: Float = 1.0
     
+    init(bossName: String, bossDef: Float, bossDmgReduction: Float, bossHp: Float, bossDmg: Float, bossDmgSuper: Float) {
+        self.bossName = bossName
+        self.bossDef = bossDef
+        self.bossDmgReduction = bossDmgReduction
+        self.bossHp = bossHp
+        self.bossDmg = bossDmg
+        self.bossDmgSuper = bossDmgSuper
+    }
+    
 }
+func dropDecimal(_ num: Float) -> Float{ //updated naming scheme from drop_decimal to dropDecimal
+    return Float(Int(num))
+}//this function drops the decimal off floating numbers and returns them to being a float like the game does giving more close to accurate calculations and data
 
-
-var vegito = Unit() //setting up "vegito" as a class unit.
-vegito.name = "Vegito"
-vegito.atk = 19975.0
-vegito.leaderSkill = 5.4
-vegito.passive = 3.0
-vegito.links = 1.7
-vegito.item = 1.0
-vegito.ki = 2.0
-vegito.passive2 = 3.0
-vegito.superatk = 6.05
-
-var goku = Unit()
-goku.name = "Goku"//setting up unit names for printing
-goku.atk = 16935.0
-goku.leaderSkill = 5.4
-goku.passive = 3.0
-goku.links = 1.8
-goku.item = 1.0
-goku.ki = 2.0
-goku.passive2 = 3.4
-goku.superatk = 6.05
-
-var gotenks = Unit()
-gotenks.name = "Gotenks"
-gotenks.atk = 17360.0
-gotenks.leaderSkill = 5.4
-gotenks.passive = 3.0
-gotenks.links = 1.55
-gotenks.item = 1.0
-gotenks.ki = 2.0
-gotenks.passive2 = 4.31
-gotenks.superatk = 6.2
+import Foundation //I am keeping this here as its the only use of it so far
+func formatNumbers(_ num: Int) -> String{
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+    var formattedNumber = formatter.string(from: NSNumber(value: num)) ?? "\(num)" //the question marks handle "Nil-Coalescing" to handle how NSNumber makes the number an optional
+    return String(formattedNumber)
+}//This function makes my own instance of NumberFormatter to use, sets it to .decimal then using Foundation and NSNumber it converts the number into a string with the commas.
 
 
 
-var kidBuu = Boss() //setting up "kidbuu" as a boss
-kidBuu.bossName = "Kid Buu"
-kidBuu.bossDef = 650000.0
-kidBuu.bossDmgReduction = 0.25
-kidBuu.bossHp = 90000000.0
 
-var broly = Boss()
-broly.bossName = "Broly"
-broly.bossDef = 81000
-broly.bossDmgReduction = 0.3
-broly.bossHp = 50000000
+var vegito = Unit(name: "Vegito", atk: 19975.0, leaderSkill: 5.4, passive: 3.0, links: 1.7, item: 1.0, ki: 2.0, passive2: 3.0, superatk: 6.05) //setting up "vegito" as a class unit.
+//vegito.name = "Vegito"
+//vegito.atk = 19975.0
+//vegito.leaderSkill = 5.4
+//vegito.passive = 3.0
+//vegito.links = 1.7
+//vegito.item = 1.0
+//vegito.ki = 2.0
+//vegito.passive2 = 3.0
+//vegito.superatk = 6.05
+
+var goku = Unit(name: "Goku", atk: 16935.0, leaderSkill: 5.4, passive: 3.0, links: 1.8, item: 1.0, ki: 2.0, passive2: 3.4, superatk: 6.05)
+//goku.name = "Goku"//setting up unit names for printing
+//goku.atk = 16935.0
+//goku.leaderSkill = 5.4
+//goku.passive = 3.0
+//goku.links = 1.8
+//goku.item = 1.0
+//goku.ki = 2.0
+//goku.passive2 = 3.4
+//goku.superatk = 6.05
+
+var gotenks = Unit(name: "Gotenks", atk: 17360.0, leaderSkill: 5.4, passive: 3.0, links: 1.55, item: 1.0, ki: 2.0, passive2: 4.31, superatk: 6.2)
+//gotenks.name = "Gotenks"
+//gotenks.atk = 17360.0
+//gotenks.leaderSkill = 5.4
+//gotenks.passive = 3.0
+//gotenks.links = 1.55
+//gotenks.item = 1.0
+//gotenks.ki = 2.0
+//gotenks.passive2 = 4.31
+//gotenks.superatk = 6.2
+
+var fauxName = "Super Saiyan 9001 Gogito"
+var fauxAtk: Float = 99999.0
+var fauxLeader: Float = 10.0
+var fauxPassive: Float = 50.0
+var fauxLinks: Float = 2.0
+var fauxItems: Float = 1.0
+var fauxKi: Float = 2.0
+var fauxPassive2: Float = 20.0
+var fauxSuperAtk: Float = 10.0
+
+var fauxUnit = Unit(name: fauxName, atk: fauxAtk, leaderSkill: fauxLeader, passive: fauxPassive, links: fauxLinks, item: fauxItems, ki: fauxKi, passive2: fauxPassive2, superatk: fauxSuperAtk)
 
 
+
+var kidBuu = Boss(bossName: "Kid Buu", bossDef: 650000, bossDmgReduction: 0.25, bossHp: 90000000, bossDmg: 1, bossDmgSuper: 1) //setting up "kidbuu" as a boss
+//kidBuu.bossName = "Kid Buu"
+//kidBuu.bossDef = 650000.0
+//kidBuu.bossDmgReduction = 0.25
+//kidBuu.bossHp = 90000000.0
+
+var broly = Boss(bossName: "Broly", bossDef: 81000, bossDmgReduction: 0.3, bossHp: 50000000, bossDmg: 1, bossDmgSuper: 1)
+//broly.bossName = "Broly"
+//broly.bossDef = 81000
+//broly.bossDmgReduction = 0.3
+//broly.bossHp = 50000000
+
+var fauxBossName = "Base Cabba"
+var fauxBossDef: Float = 9
+var fauxBossDmgReduction: Float = 0.01
+var fauxBossHp: Float = 10000000000
+var fauxBossDmg: Float = 50000000
+var fauxBossSuper: Float = 1000000000
+
+var fauxBoss = Boss(bossName: fauxBossName, bossDef: fauxBossDef, bossDmgReduction: fauxBossDmgReduction, bossHp: fauxBossHp, bossDmg: fauxBossDmg, bossDmgSuper: fauxBossSuper)
 
 //print(String(format: "%.2f", vegito.calcAtkStat()))
 
@@ -112,7 +153,7 @@ func dmgVsBoss(_ Unit: Unit, _ Boss: Boss) {
     var dmgDealt = Unit.calcAtkStat() - Boss.bossDef
     var dmgDealtNormal = dmgDealt * Boss.bossDmgReduction
     var dmgDealtSE = dmgDealtNormal * 1.5
-    var dmgDealtCrit = Unit.calcAtkStat() * Boss.bossDmgReduction * 1.9
+    var dmgDealtCrit = Unit.calcAtkStat() * Boss.bossDmgReduction * 1.9 //1.9 is the crit multiplier, make it a global
     var varDmgDealtNormal = dmgDealtNormal * dokkanVariance
     var varDmgDealtSE = dmgDealtSE * dokkanVariance
     var varDmgDealtCrit = dmgDealtCrit * dokkanVariance
@@ -142,12 +183,19 @@ func dmgPerRotation(_ Unit1: Unit, _ Unit2: Unit, _ Unit3: Unit, _ Boss: Boss){ 
     var percentHp = Float(100.0)
     var percentRemaining = (Boss.bossHp - Float(rotationDmg)) / Boss.bossHp * 100.0
     var percentDealt = percentHp - percentRemaining
-    
-    print("You did \(formattedRotationDmg) this turn! The boss's health is now \(formattedRemainingHp)")
+    if Int(remainingHp) > 0 {
+        print("You did \(formattedRotationDmg) this turn! \(Boss.bossName)'s health is now \(formattedRemainingHp)")
+    }
+    else{
+        print("You did \(formattedRotationDmg) this turn! \(Boss.bossName)'s  health is now 0!")
+    }
     var formattedPercentDealt = String(format: "%.2f", percentDealt)
     var formattedPercentRemaining = String(format: "%.2f", percentRemaining)
     var rotationsNeeded = Int(ceil(percentRemaining / percentDealt))
-    if rotationsNeeded == 1{ //this if else handles grammar
+    if rotationsNeeded == 0{ //this if else if else handles grammar also a satisfying note if you one shot it.
+        print("You one shot the boss!")
+    }
+    else if rotationsNeeded == 1{
         print("You did \(formattedPercentDealt)%  of the boss's hp this turn! The boss has \(formattedPercentRemaining)% remaining. \(rotationsNeeded) more turn like that and you win!")
     }
     else{
@@ -162,3 +210,9 @@ dmgVsBoss(vegito, kidBuu)
 
 dmgVsBoss(goku, broly)
 // this shows the function with a different unit and boss
+
+dmgVsBoss(fauxUnit, fauxBoss)
+
+dmgPerRotation(fauxUnit, vegito, gotenks, fauxBoss)
+dmgPerRotation(fauxUnit, goku, gotenks, kidBuu)
+dmgPerRotation(vegito, goku, gotenks, fauxBoss)
