b030e4cde9c902d68dc1dea8c83706685745e658
JacobMatsumoto <jmatsumoto@students.mchenry.edu>
2025-05-11 10:51:12 -0500

    final version

--- dokkan2.5.swift
+++ dokkan2.5.swift
@@ -6,10 +6,10 @@ import UIKit
 //Int > Teq
 //Teq > Agl
 
-let critMulti:Float = 1.9
-let superEffective:Float = 1.5
+let critMulti: Float = 1.9
+let superEffective: Float = 1.5
 let dokkanVariance: Float = 1.03
-let unitGuard: Float = 0.45
+//let unitGuard: Float = 0.5 units with guard have 0.5 innate,
 
 
 
@@ -80,7 +80,7 @@ class Unit {
         return attack
     }
     
-    func calcDefStat() -> Float{
+    func calcDefStatAfterAtk() -> Float{
         var defense = dropDecimal(self.unitDef * self.unitDefLead)
         defense = dropDecimal(defense * self.unitDefPassive)
         defense = dropDecimal(defense * self.unitDefLink)
@@ -89,13 +89,21 @@ class Unit {
         return defense
     }
     
+    func calcDefStatBeforeAtk() -> Float{
+        var defense = dropDecimal(self.unitDef * self.unitDefLead)
+        defense = dropDecimal(defense * self.unitDefPassive)
+        defense = dropDecimal(defense * self.unitDefLink)
+        return defense
+    }
+    
     
     func dmgPerUnit(_ Unit: Unit, _ Boss: Boss) -> Int {
+        let (typeMulti, guardMulti) = typeCalc(Unit, Boss)
         var dmgDealt = Unit.calcAtkStat() - Boss.bossDef
-        var dmgDealtNormal = dmgDealt * Boss.bossDmgReduction
+        var dmgDealtNormal = dmgDealt * Boss.bossDmgReduction * typeMulti * guardMulti
         var dmgDealtSE = dmgDealtNormal * superEffective
         var dmgDealtCrit = Unit.calcAtkStat() * Boss.bossDmgReduction * critMulti
-        var varDmgDealtNormal = dmgDealt * dokkanVariance
+        var varDmgDealtNormal = dmgDealt * dokkanVariance * typeMulti * guardMulti //more accurate damage calcs
         var varDmgDealtSe = dmgDealtSE * dokkanVariance
         var varDmgDealtCrit = dmgDealtCrit * dokkanVariance
             
@@ -145,7 +153,7 @@ func formatNumbers(_ num: Int) -> String{
 var gogeta = Unit(unitClass: true, hasGuard: false, unitType: "Teq", name: "Gogeta", atk: 21525.0, leaderSkill: 4.4, passive: 4.0, links: 1.55, item: 1.0, ki: 2.0, passive2: 1.5, superatk: 6.95, unitDef: 15925.0, unitDefLead: 4.4, unitDefPassive: 3.9, unitDefPassive2: 1.5, unitDefOnSuper: 1.5, unitDefLink: 1.05, unitDmgRedItem: 0.0, unitDmgRed: 0.60)
 //note stacking units do not benefit from the stack on their first attack gaining it, they do gain it at the end of the super example being on an additional attack.
 //var vegito = Unit(name: "Vegito", atk: 19975.0, leaderSkill: 5.4, passive: 3.0, links: 1.7, item: 1.0, ki: 2.0, passive2: 3.0, superatk: 6.05) //setting up "vegito" as a class unit.
-var vegito = Unit(unitClass: true, hasGuard: false, unitType: "Teq", name: "Vegito", atk: 19975.0, leaderSkill: 5.4, passive: 3.0, links: 1.7, item: 1.0, ki: 2.0, passive2: 3.0, superatk: 6.05, unitDef: 13888, unitDefLead: 5.4, unitDefPassive: 3.4, unitDefPassive2: 1.0, unitDefOnSuper: 2.0, unitDefLink: 1.05, unitDmgRedItem: 0.0, unitDmgRed: 50.0)
+var vegito = Unit(unitClass: true, hasGuard: false, unitType: "Teq", name: "Vegito", atk: 19975.0, leaderSkill: 5.4, passive: 3.0, links: 1.7, item: 1.0, ki: 2.0, passive2: 3.0, superatk: 6.05, unitDef: 13888, unitDefLead: 5.4, unitDefPassive: 3.4, unitDefPassive2: 1.0, unitDefOnSuper: 2.0, unitDefLink: 1.05, unitDmgRedItem: 0.0, unitDmgRed: 0.50)
 //vegito.name = "Vegito"
 //vegito.atk = 19975.0
 //vegito.leaderSkill = 5.4
@@ -155,7 +163,7 @@ var vegito = Unit(unitClass: true, hasGuard: false, unitType: "Teq", name: "Vegi
 //vegito.ki = 2.0
 //vegito.passive2 = 3.0
 //vegito.superatk = 6.05
-
+var goku = Unit(unitClass: true, hasGuard: true, unitType: "Agl", name: "Ssj 3 Kid Goku", atk: 16935.0, leaderSkill: 5.4, passive: 3.0, links: 1.8, item: 1.0, ki: 2.0, passive2: 3.4, superatk: 3.4, unitDef: 12506.0, unitDefLead: 5.4, unitDefPassive: 3.0, unitDefPassive2: 3.4, unitDefOnSuper: 1.5, unitDefLink: 1.25, unitDmgRedItem: 0.0, unitDmgRed: 0.0)
 //var goku = Unit(name: "Goku", atk: 16935.0, leaderSkill: 5.4, passive: 3.0, links: 1.8, item: 1.0, ki: 2.0, passive2: 3.4, superatk: 6.05)
 //goku.name = "Goku"//setting up unit names for printing
 //goku.atk = 16935.0
@@ -191,7 +199,8 @@ var fauxSuperAtk: Float = 10.0
 //var fauxUnit = Unit(name: fauxName, atk: fauxAtk, leaderSkill: fauxLeader, passive: fauxPassive, links: fauxLinks, item: fauxItems, ki: fauxKi, passive2: fauxPassive2, superatk: fauxSuperAtk)
 
 
-var kidBuu = Boss(bossName: "Kid Buu", bossClass: false, bossSE: true, bossType: "Int", bossDef: 650000, bossDmgReduction: 0.25, bossHp: 90000000, bossDmg: 650000.0, bossDmgSuper: 2275000.0)
+var kidBuu = Boss(bossName: "Kid Buu", bossClass: false, bossSE: true, bossType: "Str", bossDef: 650000, bossDmgReduction: 0.25, bossHp: 90000000, bossDmg: 650000.0, bossDmgSuper: 2275000.0)
+var kidBuuTest = Boss(bossName: "Kid Buu", bossClass: false, bossSE: true, bossType: "Str", bossDef: 1650000, bossDmgReduction: 0.25, bossHp: 90000000, bossDmg: 1000000.0, bossDmgSuper: 2275000.0)
 //kidBuu.bossName = "Kid Buu"
 //kidBuu.bossDef = 650000.0
 //kidBuu.bossDmgReduction = 0.25
@@ -235,7 +244,12 @@ func dmgVsBoss(_ Unit: Unit, _ Boss: Boss) {
 //This function takes a classed Unit and Boss and uses these to calculate the dmg you would deal to the boss
 //A revised one might want to take 3 units and calculate the damage dealt in a turn by the three characters in the single turn then calculate about how many turns it might take to deplete a boss's hp ...look down
 
-
+func dmgToBoss(_ Unit1: Unit, _ Unit2: Unit, _ Unit3: Unit, _ Boss: Boss) -> Int{
+    var unit1Dmg = Unit1.dmgPerUnit(Unit1, Boss)
+    var unit2Dmg = Unit2.dmgPerUnit(Unit2, Boss)
+    var unit3Dmg = Unit3.dmgPerUnit(Unit3, Boss)
+    return (unit1Dmg + unit2Dmg + unit3Dmg)
+}
 
 func dmgPerRotation(_ Unit1: Unit, _ Unit2: Unit, _ Unit3: Unit, _ Boss: Boss){ //This function will take an average of 3 units dmg and present their dmg and then
     var unit1Dmg = Unit1.dmgPerUnit(Unit1, Boss)
@@ -268,37 +282,197 @@ func dmgPerRotation(_ Unit1: Unit, _ Unit2: Unit, _ Unit3: Unit, _ Boss: Boss){ 
     }
 }
 
-func typeCalc(_ Unit: Unit, _ Boss: Boss) -> String { //if the unit's dictionary value is the boss's type we get advantage, if the boss's dictionary matches the unit's we get disadvantage anything else returns neutral. It is a weird sort of rock paper scissors
-    
+
+//bellow @MainActor was added by playgrounds as a "fix" on my return
+func typeCalc(_ Unit: Unit, _ Boss: Boss) -> (Float, Float){ //if the unit's dictionary value is the boss's type we get advantage, if the boss's dictionary matches the unit's we get disadvantage anything else returns neutral. It is a weird sort of rock paper scissors
+    var currentCalcForType = "Type"
     let typeAdvantages: [String: String] = ["Agl": "Str", "Str": "Phy", "Phy": "Int", "Int": "Teq", "Teq": "Agl"]
 
     if Unit.unitType == Boss.bossType {
-        return "Neutral"
+        currentCalcForType = "Neutral"
     } else if typeAdvantages[Unit.unitType] == Boss.bossType { //if the dictionary value of the unit's type matches the boss' type that gives advantage
-        return "Advantage"
+        currentCalcForType = "Advantage"
     } else if typeAdvantages[Boss.bossType] == Unit.unitType { //if the dictionary value of the boss's type is the unit's type it gets advantage
-        return "Disadvantage"
+        currentCalcForType = "Disadvantage"
     } else {
-        return "Neutral" //same type and non advantage/disadvantage returns neutral
+        currentCalcForType = "Neutral" //same type and non advantage/disadvantage returns neutral
     }
+    
+    //part two of the calc below
+    var guardMulti: Float = 1.0
+    var typeMulti: Float = 1.0
+    
+    if (currentCalcForType == "Neutral" && Unit.unitClass != Boss.bossClass){
+        typeMulti = 1.15
+        guardMulti = 1.0
+    }
+    if (currentCalcForType == "Neutral" && Unit.unitClass == Boss.bossClass){
+        typeMulti = 1.0
+        guardMulti = 1.0
+    }
+    if (currentCalcForType == "Advantage" && Unit.unitClass != Boss.bossClass){
+        typeMulti = 1.0
+        guardMulti = 0.5
+    }
+    if (currentCalcForType == "Advantage" && Unit.unitClass == Boss.bossClass){
+        typeMulti = 0.9
+        guardMulti = 0.5
+    }
+    if (currentCalcForType == "Disadvantage" && Unit.unitClass != Boss.bossClass){
+        typeMulti = 1.5
+        guardMulti = 1.0
+    }
+    if (currentCalcForType == "Disadvantage" && Unit.unitClass == Boss.bossClass){
+        typeMulti = 1.25
+        guardMulti = 1.0
+    }
+    return(typeMulti, guardMulti)
 }
 
 
-//func dmgTaken(_ Unit: Unit, _ Boss: Boss){
-//    var type: Float = 1.0
-//    if Unit.hasGuard == true{
-//        type = unitGuard
-//    }
-//    else if Boss.bossSE == true {
-//        type = 1.5
-//    }
-//    
-//        
-//        
-//    var unitDef = Unit.calcDefStat()
-//    var dmgTaken = (Boss.bossDmg * Unit.unitDmgRed)
-//    dmgTaken = dmgTaken * type
-//}
+
+func dmgTakenNormalBeforeAttacking(_ Unit: Unit, _ Boss: Boss)-> Int{
+    var bossVariance: Float = 1.015
+    let typeMulti: Float
+    let guardMulti: Float
+    if Unit.hasGuard == true{
+        typeMulti = 0.8
+        guardMulti = 0.5
+    }
+    else{
+        (typeMulti, guardMulti) = typeCalc(Unit, Boss)
+    }
+    var unitDefense = Unit.calcDefStatBeforeAtk()
+    var damageTaken = (Boss.bossDmg * bossVariance * typeMulti * guardMulti * (1 - Unit.unitDmgRed)) - unitDefense
+    damageTaken = dropDecimal(damageTaken)
+    if damageTaken < 0 {
+        damageTaken = 100
+    }
+    return Int(damageTaken)
+//    print(damageTaken) //to test
+}
+func dmgTakenSuperBeforeAttacking(_ Unit: Unit, _ Boss: Boss)-> Int{
+    var bossVariance: Float = 1.015
+    let typeMulti: Float
+    let guardMulti: Float
+    if Unit.hasGuard == true{
+        typeMulti = 0.8
+        guardMulti = 0.5
+    }
+    else{
+        (typeMulti, guardMulti) = typeCalc(Unit, Boss)
+    }
+    var unitDefense = Unit.calcDefStatBeforeAtk()
+    var damageTaken = (Boss.bossDmgSuper * bossVariance * typeMulti * guardMulti * (1 - Unit.unitDmgRed)) - unitDefense
+    damageTaken = dropDecimal(damageTaken)
+    if damageTaken < 0 {
+        damageTaken = 100
+    }
+    return Int(damageTaken)
+//    print(damageTaken) //to test
+}
+func dmgTakenNormalAfterAttacking(_ Unit: Unit, _ Boss: Boss)-> Int{
+    var bossVariance: Float = 1.015
+    let typeMulti: Float
+    let guardMulti: Float
+    if Unit.hasGuard == true{
+        typeMulti = 0.8
+        guardMulti = 0.5
+    }
+    else{
+        (typeMulti, guardMulti) = typeCalc(Unit, Boss)
+    }
+    var unitDefense = Unit.calcDefStatAfterAtk()
+    var damageTaken = (Boss.bossDmg * bossVariance * typeMulti * guardMulti * (1 - Unit.unitDmgRed)) - unitDefense
+    damageTaken = dropDecimal(damageTaken)
+    if damageTaken < 0 {
+        damageTaken = 100
+    }
+    return Int(damageTaken)
+//    print(damageTaken) //to test
+}
+func dmgTakenSuperAfterAttacking(_ Unit: Unit, _ Boss: Boss)-> Int{
+    var bossVariance: Float = 1.015
+    let typeMulti: Float
+    let guardMulti: Float
+    if Unit.hasGuard == true{
+        typeMulti = 0.8
+        guardMulti = 0.5
+    }
+    else{
+        (typeMulti, guardMulti) = typeCalc(Unit, Boss)
+    }
+    var unitDefense = Unit.calcDefStatAfterAtk()
+    var damageTaken = (Boss.bossDmgSuper * bossVariance * typeMulti * guardMulti * (1 - Unit.unitDmgRed)) - unitDefense
+    damageTaken = dropDecimal(damageTaken)
+    if damageTaken < 0 {
+        damageTaken = 100
+    }
+    return Int(damageTaken)
+//    print(damageTaken) //to test
+}
+
+func turnCalc(_ UnitSlotOne: Unit,_ UnitSlotTwo: Unit,_ UnitSlotThree: Unit,_ Boss: Boss){
+    var CurrentBossHp = Int(Boss.bossHp)
+    var CurrentPlayerHp = 700000
+    while (CurrentBossHp != 0) || (CurrentPlayerHp != 0){
+        var numberOfAttacks = 0
+        var superAtk = Int.random(in: 0..<5)
+        var attackPlacement = [Int]()
+        while numberOfAttacks < 10{
+            var attack = Int.random(in: 1..<5)
+            attackPlacement.append(attack)
+            numberOfAttacks += 1
+        }
+        var damageTaken = 0
+        for attack in attackPlacement{
+            if attack == 1{
+                damageTaken += dmgTakenNormalBeforeAttacking(UnitSlotOne, Boss)
+            }
+            else if attack == 2{
+                damageTaken += dmgTakenNormalAfterAttacking(UnitSlotOne, Boss)
+            }
+            else if attack == 3{
+                damageTaken += dmgTakenNormalAfterAttacking(UnitSlotTwo, Boss)
+            }
+            else if attack == 4{
+                damageTaken += dmgTakenNormalAfterAttacking(UnitSlotThree, Boss)
+            }
+        }
+        if superAtk == 0{
+            print("No super attack taken this turn!")
+        }
+        else if superAtk == 1{
+            damageTaken += dmgTakenSuperBeforeAttacking(UnitSlotOne, Boss)
+        }
+        else if superAtk == 2{
+            damageTaken += dmgTakenSuperAfterAttacking(UnitSlotOne, Boss)
+        }
+        else if superAtk == 3{
+            damageTaken += dmgTakenSuperAfterAttacking(UnitSlotTwo, Boss)
+        }
+        else if superAtk == 4{
+            damageTaken += dmgTakenSuperAfterAttacking(UnitSlotThree, Boss)
+        }
+        
+        var dmgDealtToBoss = dmgToBoss(UnitSlotOne, UnitSlotTwo, UnitSlotThree, Boss)
+        CurrentPlayerHp -= damageTaken
+        CurrentBossHp -= dmgDealtToBoss
+        if CurrentBossHp < 0{
+            CurrentBossHp = 0
+            print("You defeated \(Boss.bossName)")
+            break
+        }
+        else if CurrentPlayerHp < 0{
+            CurrentPlayerHp = 0
+            print("You took \(damageTaken) damage. You were defeated")
+            break
+        }
+        else{
+            print("You took \(damageTaken) damage this turn your hp is now \(CurrentPlayerHp). You dealt \(dmgDealtToBoss) damage the boss's hp is now \(CurrentBossHp)")
+        }
+    }
+}
 //
 //dmgPerRotation(vegito, goku, gotenks, kidBuu)
 //
@@ -316,7 +490,9 @@ func typeCalc(_ Unit: Unit, _ Boss: Boss) -> String { //if the unit's dictionary
 
 //vegito.calcAtkStat()
 //vegito.calcDefStat()
-print(formatNumbers(Int(gogeta.calcAtkStat())))
-gogeta.calcDefStat()
-print(formatNumbers(Int(vegito.calcAtkStat())))
-typeCalc(gogeta, kidBuu)
+//print(formatNumbers(Int(gogeta.calcAtkStat())))
+//gogeta.calcDefStatAfterAtk()
+//print(formatNumbers(Int(vegito.calcAtkStat())))
+//typeCalc(gogeta, kidBuu)
+//dmgTakenNormalBeforeAttacking(gogeta, kidBuu)
+turnCalc(goku, vegito, gogeta, kidBuu)
